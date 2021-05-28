265fbd22a656952017eeeb7fd2b85ebc5816da64
Evgeny Kuzin <ekuzin@outlook.com>
2021-05-28 10:57:39 +0700

    update index.php, create src/Runner.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo 'Hello!';
-echo 'World!';
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
